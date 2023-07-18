cd1b476c23b4ba79faedf4295ed87853be42c251
FlashShri <shrikantdhengle3101@gmail.com>
2023-07-18 21:22:18 +0530

    adding fields

---
+++ EmailAdminApp/src/Emal.java
@@ -0,0 +1,9 @@
+public class Emal {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+    
+}
