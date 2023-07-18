9d51e52c9e78e110b1f01d40bdff24c2fa61de4b
FlashShri <shrikantdhengle3101@gmail.com>
2023-07-18 22:49:02 +0530

    adding getter setter

--- EmailAdminApp/src/Email.java
+++ EmailAdminApp/src/Email.java
@@ -7,7 +7,7 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
-    private int mailboxCapacity;
+    private int mailboxCapacity = 500;
     private String alternateEmail;
     private int defaultPasswordLength = 10;
     private String companySuffix = "tesla.com";
@@ -40,12 +40,13 @@ public class Email {
         System.out.println("Enter the department code:\n1 for sales\n2 for development\n3 for accounts\n0 for more");
         Scanner sc = new Scanner(System.in);
         this.deptchioce = sc.nextInt();
-
+        sc.close();
         if(deptchioce ==1) {return "Sales";}
         else if(deptchioce == 2){return "Development";}
         else if(deptchioce == 3){return "Accounts";}
         else if(deptchioce == 0){return "staff";}
         else return " ";
+        
     }
 
     //generating email
@@ -79,4 +80,30 @@ public class Email {
         return new String(password); // as password is char array to convert it as String
     }
     
+    //set mailbox capacity
+    public void setMailboxCapacity(int capacity){
+        this.mailboxCapacity = capacity;
+    }
+    //set Alternate Email
+    public void setAltEmail(String AltEmai){
+        this.alternateEmail = AltEmai;
+    }
+    //set new password 
+    public void changePassowrd(String password){
+        this.password = password;
+    }
+    
+
+    //get Alternate email
+    public String getAltEmail(){
+        return alternateEmail;
+    }
+    //get MailBox Capacity
+    public int getMailboxCapacity(){
+        return mailboxCapacity;
+    }
+    //get password
+    public String getPassword(){
+        return password;
+    }
 }
