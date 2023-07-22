6043510b4f93b50e6e03ecfddcf3231dca4760f4
FlashShri <shrikantdhengle3101@gmail.com>
2023-07-22 19:43:46 +0530

    adding menu bar

--- TextLime/src/TextLime.java
+++ TextLime/src/TextLime.java
@@ -1,4 +1,7 @@
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.WindowConstants;
 
 public class TextLime  extends JFrame{
@@ -7,9 +10,27 @@ public class TextLime  extends JFrame{
         setSize(750, 850);
         setTitle("TextLime Editor");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
- 
-
         
+        // creatint MENU BAR
+        JMenuBar menu_bar = new JMenuBar(); 
+
+        //add menu bar to Frame
+        setJMenuBar(menu_bar);
+
+        //creating menu
+        //for that we need instance of JMenu class
+
+        JMenu file = new JMenu("File");
+        JMenu edit = new JMenu("Edit");
+        JMenu help = new JMenu("Help");
+
+        //adding menu in menu bar
+        menu_bar.add(file);
+        menu_bar.add(edit);
+        menu_bar.add(help);
+
+       
+
         setVisible(true);
 
 
