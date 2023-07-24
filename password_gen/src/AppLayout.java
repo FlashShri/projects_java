d2bae22bd0e90c2193f44776e9b394563c393d48
FlashShri <shrikantdhengle3101@gmail.com>
2023-07-24 20:02:58 +0530

    adding gui layout

--- password_gen/src/AppLayout.java
+++ password_gen/src/AppLayout.java
@@ -1,3 +1,4 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -6,9 +7,22 @@ public class AppLayout extends JFrame {
 
     public AppLayout(){
         JLabel label = new JLabel("Password Generator");
-        label.setBounds(50, 50, 150, 20);
+        label.setBounds(60, 50, 150, 20);
+
+        JLabel label2 = new JLabel("Click here to generate new password =>");
+        label2.setBounds(60, 70, 300, 20);
+
+        JButton b = new JButton("New");
+        b.setBounds(60, 100, 85, 30);
+
+        JLabel passField = new JLabel("Absjd@3453");
+        passField.setBounds(60, 130, 300, 30);
+        
         
         add(label);
+        add(label2);
+        add(b);
+        add(passField);
 
         setSize(450, 650);
         setLayout(null);
